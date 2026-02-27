5bb319396f15e3f8a3ce612938a7fe8a5a83af1a
Aqsu25 <aqsu1325@gmail.com>
2026-02-26 23:51:21 -0800

    user

--- Ecommerce/frontend/src/components/common/Breadcrum.jsx
+++ Ecommerce/frontend/src/components/common/Breadcrum.jsx
@@ -6,7 +6,7 @@ function Breadcrum({ bread }) {
         <nav aria-label="breadcrumb" className="w-max my-2">
             <ol className="flex flex-wrap items-center rounded-md py-2">
                 <li className="flex items-center text-sm text-slate-500 hover:text-[#007595]">
-                    <Link to="#" className='text-black hover:text-[#007595]'>Home</Link>
+                    <Link to="/" className='text-black hover:text-[#007595]'>Home</Link>
                     <span className="pointer-events-none mx-2 text-gray-400">/</span>
                 </li>
                 <li className="flex items-center text-sm text-slate-500 hover:text-[#007595]">
